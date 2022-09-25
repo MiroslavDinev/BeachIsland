2bcfa050244ffb58db0e291698349434194e4e12
Miroslav Dinev <miroslavdinev@yahoo.com>
2022-09-25 16:40:33 +0300

    Description hide show work in progress

--- BeachIsland.Client/beachisland/src/app/feature/islands/island-details/island-details.component.ts
+++ BeachIsland.Client/beachisland/src/app/feature/islands/island-details/island-details.component.ts
@@ -58,4 +58,13 @@ export class IslandDetailsComponent implements OnInit {
   cancelChanges(){
     this.router.navigate(['islands']);
   }
+
+  toggleDescription() {
+    var x = document.getElementById("myDIV");
+  if (x!.style.display === "none") {
+    x!.style.display = "block";
+  } else {
+    x!.style.display = "none";
+  }
+  }
 }
